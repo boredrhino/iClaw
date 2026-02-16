1f7299d52f5bd945a905421a8938176d99f5a2d9
boredrhino <267508+boredrhino@users.noreply.github.com>
2026-02-15 23:55:01 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,9 @@
-// Constants like bundle IDs
+// src/constants.ts - App bundle IDs, common selectors, etc. for iOS
+export const BUNDLE_IDS = {
+  SAFARI: 'com.apple.mobilesafari',
+  MESSAGES: 'com.apple.MobileSMS',
+  SETTINGS: 'com.apple.Preferences',
+  // Add more as needed
+};
 
+console.log("iOS constants loaded");
